75d66dd464a3be2d24c74eb66d593dd45fb1e7f0
caramelbomb <caramelza@hotmail.com>
2021-04-19 23:08:03 +0700

    connect firebase and test

---
+++ config.js
@@ -0,0 +1,14 @@
+const firebaseConfig = {
+  apiKey: 'AIzaSyC_dxura1XRZyfRX5HdZ8te6ySt1G1ZFuo',
+  authDomain: 'tails-b6a83.firebaseapp.com',
+  databaseURL: 'https://tails-b6a83-default-rtdb.firebaseio.com',
+  projectId: 'tails-b6a83',
+  storageBucket: 'tails-b6a83.appspot.com',
+  messagingSenderId: '796796794461',
+  appId: '1:796796794461:web:66c1651664e8b8a959bfb4',
+  measurementId: 'G-Z1RY21PLC1'
+}
+
+module.exports = {
+  firebaseConfig
+}
--- index.js
+++ index.js
@@ -1,8 +1,22 @@
 const express = require('express')
 const app = express()
+const PORT = 9000
 
 app.get('/health', (req, res) => {
-  res.send('test')
+  res.send('OK')
 })
 
-app.listen(9000)
+const db = require('./firebase')
+
+app.get('/firebase', async (req, res) => {
+  const result = await db.collection('test').doc('document').update({ test: 'success' })
+  if (result) {
+    res.status(200).json({ message: 'success' })
+  } else {
+    res.status(400).json({ message: 'fail' })
+  }
+})
+
+app.listen(PORT, () => {
+  console.log('connecting port: ', PORT)
+})
