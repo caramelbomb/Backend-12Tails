61fd931864f80886a67869f527af03da30655975
caramelbomb <caramelza@hotmail.com>
2021-04-22 22:21:45 +0700

    clean code

--- index.js
+++ index.js
@@ -6,25 +6,12 @@ const PORT = 9000
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
-const routes = require('./routes')
-app.use('/api', routes)
-
 app.get('/health', (req, res) => {
   res.send('OK')
 })
 
-// connect firebase
-const db = require('./firebase')
-
-// test firebase
-app.get('/firebase', async (req, res) => {
-  const result = await db.collection('test').doc('document').update({ test: 'success' })
-  if (result) {
-    res.status(200).json({ message: 'success' })
-  } else {
-    res.status(400).json({ message: 'fail' })
-  }
-})
+const routes = require('./routes')
+app.use('/api', routes)
 
 app.listen(PORT, () => {
   console.log('connecting port: ', PORT)
