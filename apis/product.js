fa05ab8e9c97883e4251904ab05da77fa9630a8e
caramelbomb <caramelza@hotmail.com>
2021-04-25 18:40:44 +0700

    finish backend

--- apis/product.js
+++ apis/product.js
@@ -4,14 +4,14 @@ const db = require('../firebase')
 const { getUser } = require('../passport')
 
 router.post('/sale', getUser(), async (req, res, next) => {
-  const { name, price, detail, salerID } = req.body
+  const { name, price, detail, salerID, type } = req.body
   try {
-    if (!name || !price || !detail || !salerID) {
+    if (!name || !price || !detail || !salerID || !type) {
       return res.status(400).json({ message: 'data incomplete' })
     }
     const products = await db.collection('products').get()
     const allProduct = products.docs.map(doc => doc.data())
-    const id = allProduct.length + 1
+    const id = allProduct.length
 
     const result = await db.collection('products').doc(`${id}`).set({
       ...req.body,
@@ -31,12 +31,12 @@ router.post('/sale', getUser(), async (req, res, next) => {
 router.put('/buy', getUser(), async (req, res, next) => {
   const { productID, buyerID } = req.body
   try {
-    if (!productID || !buyerID) {
+    if (productID === undefined || !buyerID) {
       return res.status(400).json({ message: 'data incomplete' })
     }
     const products = await db.collection('products').get()
     const allProduct = products.docs.map(doc => doc.data())
-    const findProduct = allProduct[productID - 1]
+    const findProduct = allProduct[productID]
 
     const result = await db.collection('products').doc(`${productID}`).set({
       ...findProduct,
@@ -58,12 +58,15 @@ router.get('/available', getUser(), async (req, res, next) => {
   try {
     const products = await db.collection('products').get()
     const allProduct = products.docs.map(doc => doc.data())
-    const findProduct = allProduct.filter(element => element.status === 'available')
+    const findProduct = allProduct.map((item, index) => {
+      return { _id: index, ...item }
+    })
+    const result = findProduct.filter(element => element.status === 'available')
 
-    if (!findProduct) {
+    if (!result) {
       res.status(400).json({ result: [] })
     } else {
-      res.status(200).json({ result: findProduct })
+      res.status(200).json({ result })
     }
   } catch (err) {
     next(err)
